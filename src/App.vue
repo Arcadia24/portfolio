fc398e763130468836c35f3475f247edaec95e65
Arcadia24 <nathanp.vidal@gmail.com>
2024-01-09 16:40:34 +0100

    Update with three

--- src/App.vue
+++ src/App.vue
@@ -50,8 +50,10 @@
       <span class="animate" style="--i:6;"></span>
     </div>
 
-    <div class="home-imgHover"></div>
-    <span class="animate home-img" style="--i:7;"></span>
+     <div class="home-canvas">
+      <canvas id="canvas"></canvas>
+     </div>
+
 
 
   </section>
@@ -281,8 +283,14 @@
 </template>
 
 <script>
-
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { OutputPass } from 'three/examples/jsm/postprocessing/OutputPass.js';
 export default {
+
   data() {
     return {
       isMenuOpen: false,
@@ -335,8 +343,256 @@ export default {
       this.activeSection = null;
       this.isSticky = top > 100;
     },
+    initThreeJS(){
+
+      const params = {
+          red: 1.0,
+          green: 1.0,
+          blue: 1.0,
+          treshold: 0.7,
+          strenght: 0.4,
+          radius: 0.4
+      };
+
+      // Create a scene
+      const scene = new THREE.Scene();
+
+      // Create a camera
+      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      camera.position.set(0, 0, 10);
+
+      // Import the canvas element
+      const canvas = document.getElementById('canvas');
+
+      // Create a WebGLRenderer and set its width and height
+      const renderer = new THREE.WebGLRenderer({
+          canvas: canvas,
+          // Antialiasing is used to smooth the edges of what is rendered
+          antialias: true,
+          // Activate the support of transparency
+          alpha: true
+      });
+
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      renderer.setPixelRatio( window.devicePixelRatio );
+      renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+      const composer = new EffectComposer( renderer );
+      const renderPass = new RenderPass( scene, camera );
+
+      const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ));
+      bloomPass.threshold = params.treshold;
+      bloomPass.strength = params.strenght;
+      bloomPass.radius = params.radius;
+      composer.addPass( renderPass );
+      composer.addPass( bloomPass );
+
+      const outputPass = new OutputPass();
+      composer.addPass( outputPass );
+
+      // Create the controls
+      const controls = new OrbitControls(camera, canvas);
+
+      // Handle the window resize event
+      window.addEventListener('resize', () => {
+          // Update the camera
+          camera.aspect =  window.innerWidth / window.innerHeight;
+          camera.updateProjectionMatrix();
+
+          // Update the renderer
+          renderer.setSize(window.innerWidth, window.innerHeight);
+          renderer.setPixelRatio(window.devicePixelRatio);
+          composer.setSize(window.innerWidth, window.innerHeight);
+      });
+
+      // Create the uniforms
+
+      const uniforms = {
+          u_resolution: { type: 'v2', value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
+          u_time: { type: 'f', value: 0.0 },
+          u_frequency: { type: 'f', value: 0.0 },
+      };
+
+
+      // Create the sphere
+      const geometry = new THREE.IcosahedronGeometry(4, 30);
+      const material = new THREE.ShaderMaterial({
+          uniforms,
+          vertexShader: `
+            uniform float u_time;
+            uniform float u_frequency;
+            vec3 mod289(vec3 x)
+            {
+            return x - floor(x * (1.0 / 289.0)) * 289.0;
+            }
+
+            vec4 mod289(vec4 x)
+            {
+            return x - floor(x * (1.0 / 289.0)) * 289.0;
+            }
+
+            vec4 permute(vec4 x)
+            {
+            return mod289(((x*34.0)+10.0)*x);
+            }
+
+            vec4 taylorInvSqrt(vec4 r)
+            {
+            return 1.79284291400159 - 0.85373472095314 * r;
+            }
+
+            vec3 fade(vec3 t) {
+            return t*t*t*(t*(t*6.0-15.0)+10.0);
+            }
+            float pnoise(vec3 P, vec3 rep)
+            {
+            vec3 Pi0 = mod(floor(P), rep); // Integer part, modulo period
+            vec3 Pi1 = mod(Pi0 + vec3(1.0), rep); // Integer part + 1, mod period
+            Pi0 = mod289(Pi0);
+            Pi1 = mod289(Pi1);
+            vec3 Pf0 = fract(P); // Fractional part for interpolation
+            vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+            vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+            vec4 iy = vec4(Pi0.yy, Pi1.yy);
+            vec4 iz0 = Pi0.zzzz;
+            vec4 iz1 = Pi1.zzzz;
+
+            vec4 ixy = permute(permute(ix) + iy);
+            vec4 ixy0 = permute(ixy + iz0);
+            vec4 ixy1 = permute(ixy + iz1);
+
+            vec4 gx0 = ixy0 * (1.0 / 7.0);
+            vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+            gx0 = fract(gx0);
+            vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+            vec4 sz0 = step(gz0, vec4(0.0));
+            gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+            gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+            vec4 gx1 = ixy1 * (1.0 / 7.0);
+            vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+            gx1 = fract(gx1);
+            vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+            vec4 sz1 = step(gz1, vec4(0.0));
+            gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+            gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+            vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+            vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+            vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+            vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+            vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+            vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+            vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+            vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+            vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+            g000 *= norm0.x;
+            g010 *= norm0.y;
+            g100 *= norm0.z;
+            g110 *= norm0.w;
+            vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+            g001 *= norm1.x;
+            g011 *= norm1.y;
+            g101 *= norm1.z;
+            g111 *= norm1.w;
+
+            float n000 = dot(g000, Pf0);
+            float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+            float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+            float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+            float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+            float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+            float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+            float n111 = dot(g111, Pf1);
+
+            vec3 fade_xyz = fade(Pf0);
+            vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+            vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+            float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+            return 2.2 * n_xyz;
+            }
+
+            void main() {
+                float noise = 5.0 * pnoise(position + u_time, vec3(10.0));
+                float displacement = (u_frequency / 20.0) * (noise / 30.0);
+                vec3 newPosition = position + normal * displacement;
+                gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
+            }
+          `,
+          fragmentShader: `
+            uniform float u_resolution;
+            void main() {
+            
+                vec2 st = gl_FragCoord.xy/u_resolution;
+
+                gl_FragColor = vec4(vec3(1.0,1.0, 1.0), 1.0);
+            }
+          `
+      });
+      const mesh = new THREE.Mesh(geometry, material);
+      scene.add(mesh);
+      mesh.material.wireframe = true;
+      mesh.position.x += 5;
+
+      const listener = new THREE.AudioListener();
+      camera.add(listener);
+
+      const sound = new THREE.Audio(listener);
+      const analyser = new THREE.AudioAnalyser(sound, 32);
+
+      const handleSuccess = function (stream) {
+          const audioContext = listener.context; // Use the listener's audio context
+          const mediaStreamSource = audioContext.createMediaStreamSource(stream);
+          sound.setNodeSource(mediaStreamSource);
+      };
+
+      navigator.mediaDevices
+          .getUserMedia({audio: true, video: false})
+          .then(handleSuccess)
+          .catch(e => console.error(e));
+
+      // Function to resume audio context after user interaction
+      function resumeAudioContext() {
+          if (listener.context.state === "suspended") {
+              listener.context.resume().then(() => {
+                  console.log("AudioContext resumed!");
+              }).catch(e => console.error(e));
+          }
+          // Remove the event listener after it has been called
+          document.removeEventListener('click', resumeAudioContext);
+      }
+
+      // Add the event listener for user interaction
+      document.addEventListener('click', resumeAudioContext);
+
+
+      const clock = new THREE.Clock();
+
+      // Create the animation loop
+      const animate = () => {
+          // Update the time uniform
+          uniforms.u_time.value = clock.getElapsedTime();
+          uniforms.u_frequency.value = analyser.getAverageFrequency();
+          // Call animate recursively
+          requestAnimationFrame(animate);
+
+          // Update the controls
+          controls.update();
+
+          // Update the composer
+          composer.render();
+
+          // // Render the scene
+          // renderer.render(scene, camera);
+      }
+
+      // Call animate for the first time
+      animate();
+    }
   },
   mounted() {
+    this.initThreeJS();
     window.addEventListener('scroll', this.handleScroll);
   },
   beforeUnmount() {
@@ -376,6 +632,12 @@ html {
 body {
   background-color: var(--bg-color);
   color: var(--text-color);
+}
+canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
 }
 a:visited {
   color: var(--text-color); /* Replace with your desired color */
